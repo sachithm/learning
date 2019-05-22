a8163be227f8b9f942cb0a1fbd5bf4ce77820301
Sachith Mendis <sachith.mendis@gmail.com>
2019-05-22 16:34:36 +0100

    This is after the introduction to node exercise

---
+++ Intro/echo.js
@@ -0,0 +1,9 @@
+function echo(str, num){
+    for (let index = 0; index < num; index++) {
+        console.log(str);   
+    }
+}
+
+
+echo("This is international", 4);
+echo("I am the master of node",12)
